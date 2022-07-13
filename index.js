7a59b957e85d13a7b7a5b2483af2ce23c7f8142a
Januacele <januacelevieira@hotmail.com>
2022-07-13 02:01:56 -0300

    routs poll working

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
-
+import dayjs from "dayjs";
+import joi from "joi";
 
 dotenv.config();
 const app = express();
@@ -22,9 +23,50 @@ function mongoConection() {
 mongoConection();
 
 
-app.get("/", (req, res) => {
-    // Manda como resposta o texto 'Hello World'
-    res.send('Hello World');
+app.post('/poll', async (req, res) => {
+    const {title, expireAt} = req.body;
+    let now = dayjs();
+    const date30 = now.add(1, 'M').format('YYYY-MM-DD HH:mm');
+    console.log(date30);
+
+    const pollSchema = joi.object({
+        title: joi.string().required(),
+        expireAt: joi.string().allow("")
+    });
+
+    const validate = pollSchema.validate(req.body);
+
+    if(validate.error){
+        return res.status(422).send('Título é um dado obrigatório');
+    }
+
+    let expire = date30;
+    if(expireAt.length){
+        expire = expireAt;
+    }
+
+    try {
+        const insertPoll = await db.collection('polls').insertOne({
+            title,
+            expireAt
+        });
+
+        res.status(201).send(insertPoll);
+
+    } catch (error) {
+        return res.sendStatus(500)
+    }
+});
+
+
+app.get('/poll', async (req, res) => {
+    
+    try {
+        const findPolls = await db.collection('polls').find().toArray();
+        res.status(201).send(findPolls);
+      } catch (error) {
+        res.sendStatus(500);
+      }
 });
 
 const PORT = process.env.PORT || 5001
