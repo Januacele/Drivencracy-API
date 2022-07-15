47917344285f298ffd0b1b85c18f10ef6f499518
Januacele <januacelevieira@hotmail.com>
2022-07-14 23:25:57 -0300

    change PORT at dotenv to 5000

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(router);
 
 
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
     console.log(`Server is runing on port: ${process.env.PORT}`);
